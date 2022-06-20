08222484ce296c40886067162e6fa7ea33ce9ef6
Melissa Vaiden <melissavaiden@gmail.com>
2022-06-20 16:53:08 -0400

    Completed Assignment

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About({image="https://via.placeholder.com/215", about}) {
+    return (
+        <aside>
+            <img alt='blog logo' src={image}/>
+            <p>{about}</p>
+        </aside>
+    );
+
+}
+
+export default About;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header(props) {
+    return (
+    <header>
+        <h1>{props.name}</h1>
+    </header>
+    )}
+
+export default Header;
+
+
+//App
+//  header
+//  about
+//  article list
+//      article
